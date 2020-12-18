4f516a6fc9157dfa143aa124b7ee19adeceb9ce3
Ashbeel Ghouri <ash.ghouri@techtronix.biz>
2020-12-18 07:24:23 +0500

    created an interface for types of post

--- index.ts
+++ index.ts
@@ -1,1 +1,17 @@
-const myName: string = 'Ashbeel';
+interface PostGateKeeper {
+    title: string;
+    daysOld: number;
+    published: boolean;
+}
+
+const post = {
+    title: "Latest title",
+    daysOld: 10,
+    published: false
+};
+
+const printPost = (postToPrint: PostGateKeeper) => {
+    return `${postToPrint.title} - ${postToPrint.daysOld} days old`;
+};
+
+printPost(post);
