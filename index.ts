9acf0af62c4fb864efafcf6a07b47359cc0f8dbb
Ashbeel Ghouri <ash.ghouri@techtronix.biz>
2020-12-18 07:35:57 +0500

    declaring and initializing classes

---
+++ index.ts
@@ -0,0 +1,17 @@
+class Car{
+
+    color: string;
+    year: number;
+
+    constructor (color: string, year: number) {
+        this.color = color;
+        this.year = year;
+    }
+
+    drive(){
+        console.log("Vroom!");
+    }
+};
+
+const myCar = new Car("red", 2000);
+myCar.drive();
