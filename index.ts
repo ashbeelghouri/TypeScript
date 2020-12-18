f1f29d6321e0991f1c877fb2e56414de50ed0f88
Ashbeel Ghouri <ash.ghouri@techtronix.biz>
2020-12-18 07:43:41 +0500

    public and private methods

--- index.ts
+++ index.ts
@@ -1,16 +1,25 @@
 class Car{
 
-    color: string;
-    year: number;
-
-    constructor (color: string, year: number) {
-        this.color = color;
-        this.year = year;
+    constructor (public color: string, private year: number) {
     }
 
-    drive(){
-        console.log("Vroom!");
+    public drive(){
+        console.log("car: ", this.color, "year", this.year);
+        this.putInGear();
+        this.pressPedal();
+        this.turnWheel();
     }
+
+    private putInGear(){
+        console.log("Gear put");
+    }
+    private pressPedal(){
+        console.log("pressed pedal");
+    }
+    private turnWheel(){
+        console.log("turned wheel");
+    }
+
 };
 
 const myCar = new Car("red", 2000);
