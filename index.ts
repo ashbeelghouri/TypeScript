110c90242bacf74722e3ab4ff9275f9d2de97d38
Ashbeel Ghouri <ash.ghouri@techtronix.biz>
2020-12-18 08:05:35 +0500

    interfaces implementing classes

--- index.ts
+++ index.ts
@@ -1,3 +1,21 @@
-import { Car } from './Car';
+interface Driveable{
+    speed: number;
+    drive(): string;
+}
 
-const myCar = new Car();
+class CarA implements Driveable{
+    speed = 10;
+
+    drive(){
+        console.log(`I am driving at ${this.speed}`);
+        return "" + this.speed;
+    }
+}
+
+const myCarA = new CarA();
+
+const StartDriving = (vehical: Driveable) => {
+    vehical.drive();
+};
+
+StartDriving(myCarA);
